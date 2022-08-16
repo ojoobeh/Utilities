d9a603bf756f80439dff25eaa4210dec6f13b0f4
Farzad <104139314+farzaddev65@users.noreply.github.com>
2022-08-17 02:19:51 +0430

    start project

--- lib/utilities.dart
+++ lib/utilities.dart
@@ -1,6 +1,13 @@
 
 import 'utilities_platform_interface.dart';
 
+export 'package:collection/collection.dart';
+export 'package:flutter_svg/flutter_svg.dart';
+export 'package:get/get.dart';
+export 'package:get_storage/get_storage.dart';
+export 'package:path_provider/path_provider.dart';
+
+
 class Utilities {
   Future<String?> getPlatformVersion() {
     return UtilitiesPlatform.instance.getPlatformVersion();
