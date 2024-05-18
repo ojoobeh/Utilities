28ffd8a6f1cc2839705e46acd3231ca7132626ec
Farzad <104139314+farzaddev65@users.noreply.github.com>
2024-05-18 12:02:21 +0330

    first commit

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -4,29 +4,31 @@ Widget button({
   required final VoidCallback onTap,
   final String title = "",
   final EdgeInsets margin = EdgeInsets.zero,
-  final EdgeInsets padding = const EdgeInsets.symmetric(vertical: 4, horizontal: 4),
+  final EdgeInsets padding = const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
   final Widget trailing = const SizedBox(),
   final Widget leading = const SizedBox(),
-  final double width = 150,
+  final double? width,
   final double height = 40,
   final Widget? titleWidget,
   final Color? backgroundColor,
   final double? borderRadius = 12,
+  final double? fontSize,
   final TextStyle? textStyle,
   final bool maxWidth = false,
   final Color? borderColor,
   final Color? titleColor,
 }) =>
     Container(
-      width: maxWidth ? Get.width : width,
+      width:  width,
       height: height,
       padding: margin,
       child: ElevatedButton(
         onPressed: onTap,
         style: ButtonStyle(
           padding: MaterialStateProperty.all(padding),
+
           shadowColor: MaterialStateProperty.all(backgroundColor??context.theme.primaryColorDark),
-          backgroundColor: MaterialStateProperty.all(backgroundColor),
+          backgroundColor: MaterialStateProperty.all(backgroundColor??Colors.green),
           shape: borderRadius == null
               ? null
               : MaterialStateProperty.all(
@@ -38,7 +40,7 @@ Widget button({
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             leading,
-            titleWidget ?? Text(title, style: textStyle?.copyWith(color: titleColor ?? Colors.white) ?? textTheme.bodyLarge!.copyWith(color: titleColor ?? Colors.white,fontWeight: FontWeight.bold,fontSize: 14)),
+            titleWidget ?? Text(title, style: textStyle??context.textTheme.bodyMedium!.copyWith(color: titleColor ?? Colors.white,fontSize: fontSize) ),
             trailing,
           ],
         ),
